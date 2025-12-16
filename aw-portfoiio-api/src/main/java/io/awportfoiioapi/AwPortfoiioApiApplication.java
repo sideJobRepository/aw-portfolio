029f09ebe202c71f7e25d9f22705590139d536a9
PeachCoolpis <rlglxh@naver.com>
2025-12-16 13:01:55 +0900

    백엔드 서버 세팅

---
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/AwPortfoiioApiApplication.java
@@ -0,0 +1,13 @@
+package io.awportfoiioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwPortfoiioApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AwPortfoiioApiApplication.class, args);
+    }
+    
+}
---
+++ aw-portfoiio-api/src/test/java/io/awportfoiioapi/AwPortfoiioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.awportfoiioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwPortfoiioApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
