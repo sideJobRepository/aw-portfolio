8634226e1af73803205720606470733ce7a63d9f
배성환 <baeseongwhan@ulim.co>
2025-12-19 17:54:56 +0900

    chore. 프론트 ci cd 수정

--- aw-portfoiio-front/lib/prisma.ts
+++ aw-portfoiio-front/lib/prisma.ts
@@ -6,6 +6,6 @@ export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ['query'],
-    });
+     });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
