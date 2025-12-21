1e9171b501e57733b091a067c4d4c661389244ca
PeachCoolpis <rlglxh@naver.com>
2025-12-21 23:43:44 +0900

    feat. 안내사항 엔티티 생성

---
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/notification/entity/Notification.java
@@ -0,0 +1,33 @@
+package io.awportfoiioapi.notification.entity;
+
+import io.awportfoiioapi.mapperd.DateSuperClass;
+import io.awportfoiioapi.options.entity.Options;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Table(name = "NOTIFICATION")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Notification extends DateSuperClass {
+    
+    
+    // 안내사항 ID
+    @Id
+    @Column(name = "NOTIFICATION_ID")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    
+    // 옵션 ID
+    @JoinColumn(name = "OPTIONS_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Options options;
+    
+    // 안내사항 설명
+    @Column(name = "NOTIFICATION_DESCRIPTION")
+    private String description;
+}
---
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/notification/respotiroy/NotificationRepository.java
@@ -0,0 +1,7 @@
+package io.awportfoiioapi.notification.respotiroy;
+
+import io.awportfoiioapi.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+}
