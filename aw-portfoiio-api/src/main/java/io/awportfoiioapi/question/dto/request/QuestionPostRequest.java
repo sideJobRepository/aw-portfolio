c0553ebbdd9152a9b7edd5cdc73eef52fd84772a
배성환 <baeseongwhan@ulim.co>
2025-12-22 13:41:32 +0900

    feat. 질문 검증 추가

--- aw-portfoiio-api/src/main/java/io/awportfoiioapi/question/dto/request/QuestionPostRequest.java
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/question/dto/request/QuestionPostRequest.java
@@ -1,5 +1,7 @@
 package io.awportfoiioapi.question.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,17 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class QuestionPostRequest {
+    @NotNull(message = "포트폴리오 아이디는 필수입니다.")
     private Long portfolioId;
+    @NotNull(message = "질문 단계는 필수입니다.")
     private Integer step;
+    @NotNull(message = "질문 순서는 필수입니다.")
     private Integer order;
+    @NotBlank(message = "질문 제목은 필수입니다.")
     private String title;
+    @NotBlank(message = "질문 살명은 필수입니다.")
     private String description;
+    @NotBlank(message = "질문 타입은 필수입니다.")
     private String type;
     private MultipartFile thumbnail;
     private Integer minLength;
