517cf6ccfb2d9ba8339085e2e77a52a95a36b240
배성환 <baeseongwhan@ulim.co>
2026-01-02 14:25:07 +0900

    feat. 레스트 컨트롤러 누락 수정

--- aw-portfoiio-api/src/main/java/io/awportfoiioapi/proxy/controller/ProxyController.java
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/proxy/controller/ProxyController.java
@@ -2,27 +2,27 @@ package io.awportfoiioapi.proxy.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@RestController
 public class ProxyController {
     
-    private final RestClient restClient;
-
+    
     @GetMapping("/proxy")
     public ResponseEntity<String> proxy(@RequestParam String url) {
         // http만 허용
         if (!url.startsWith("http://")) {
             return ResponseEntity.badRequest().body("Only http allowed");
         }
-
+        RestClient restClient = RestClient.create();
+        
         String body = restClient.get()
                 .uri(url)
                 .retrieve()
