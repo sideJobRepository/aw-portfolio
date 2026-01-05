c9d49356cd0b81ea5fef420e8b2bd131860606d5
Chanjonge <woal2589@naver.com>
2026-01-05 10:17:48 +0900

    메타네임 변경

--- aw-portfoiio-front/app/layout.tsx
+++ aw-portfoiio-front/app/layout.tsx
@@ -1,30 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import RecoilProvider from "@/app/RecoilProvider";
-import AuthListener from "@/app/AuthListener";
+import type { Metadata } from 'next';
+import './globals.css';
+import RecoilProvider from '@/app/RecoilProvider';
+import AuthListener from '@/app/AuthListener';
 
 export const metadata: Metadata = {
-  title: "언제나 디자인 타입형 리스트",
-  description: "Professional form management system",
+    title: '언제나 디자인',
+    description: '언제나 디자인 포트폴리오',
+    openGraph: {
+        title: '언제나 디자인',
+        description: '언제나 디자인 포트폴리오',
+        url: 'https://www.alwaysdesign.co.kr',
+    },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ko">
-      <meta
-        httpEquiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
-      ></meta>
-      <body>
-        <RecoilProvider>
-          <AuthListener />
-          {children}
-        </RecoilProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="ko">
+            <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+            <body>
+                <RecoilProvider>
+                    <AuthListener />
+                    {children}
+                </RecoilProvider>
+            </body>
+        </html>
+    );
 }
